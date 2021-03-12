e9b72d329d1d15f2a0b745616b73d5f42f2eaab2
Simon Maher <simonmaher8307@hotmail.com>
2021-03-12 15:24:12 +0200

    added destructor function, search functionality

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const IMG_PATH ='https://image.tmdb.org/t/p/w1280'
 
 const SEARCH_URL ='https://api.themoviedb.org/3/search/movie?api_key=3429c80904fb47c63b942133e4b8d98e&query="' 
 
-
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 getMovies(API_URL)
@@ -12,9 +12,49 @@ getMovies(API_URL)
 async function getMovies(url){
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data.results);
+    showMovies(data.results);
 }
 
+function showMovies(movies) {
+    main.innerHTML = ''
+
+    movies.forEach((movie) => {
+        const {title, poster_path,vote_average,overview} = movie
+
+        const movieElement = document.createElement('div')
+        movieElement.classList.add('movie')
+
+        movieElement.innerHTML = `
+      
+            <img src ="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByVote(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>
+                ${overview}
+                </h3>
+            </div>
+        
+        `
+        main.appendChild(movieElement)
+    })
+}
+
+        
+        
+        
+function getClassByVote(vote) {
+    if(vote >= 8) {
+        return 'green'
+
+    }else if(vote >= 5){
+        return 'orange'
+    }else {
+        return 'red'
+    }
+}
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
