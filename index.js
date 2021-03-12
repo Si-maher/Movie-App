8d9ffbcc96e4df318f848486aa611e7291e4652e
Simon Maher <simonmaher8307@hotmail.com>
2021-03-12 14:41:28 +0200

    added API key, async function

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3429c80904fb47c63b942133e4b8d98e&page=1'
+const API_URL ='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3429c80904fb47c63b942133e4b8d98e&page=1'
 const IMG_PATH ='https://image.tmdb.org/t/p/w1280'
-const SEARCH_URL = 'https://api.themoviedb.org/3/serch/movie? api_key=3429c80904fb47c63b942133e4b8d98e&query' 
 
+
+const SEARCH_URL ='https://api.themoviedb.org/3/search/movie?api_key=3429c80904fb47c63b942133e4b8d98e&query="' 
+
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
 getMovies(API_URL)
 
 async function getMovies(url){
-    const respond = await fetch(url)
-    const data = await respond.json()
+    const res = await fetch(url)
+    const data = await res.json()
     console.log(data.results);
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_URL + searchTerm)
+        search.value = ''
+    } else {
+        window.location.reload()
+
+    }
+})
